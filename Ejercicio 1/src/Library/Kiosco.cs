b2b74ecdcdc77fc045c947b1bb2ae31884b0747b
Augusto Taño <augusto.1812@hotmail.com>
2020-09-01 19:40:19 -0300

    Update Kiosco.cs

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -10,8 +10,9 @@ namespace Expert_SRP
             return pesos >= a.PrecioDulce + a.PrecioMasa;
         }
 
-        //Debería estar en su propia clase, encargada de convertir el dinero.
-        //Una clase propia de persona o similar que almacenara el dinero que tuviera una persona podría ser util.
+        //Debería estar en su propia clase, encargada de convertir el dinero. No cumple SRP.
+        //La clase kiosco no cumple Expert, no es ideal porque no tiene la información cómo para encargarse de esto.
+        //Una clase propia de persona o similar que almacenara el dinero que tuviera una persona podría ser util para futuros cálculos.
         private Double ConvertirAPesos(Double dinero, String moneda)
         {
             if (moneda.Equals("U$S")) 
