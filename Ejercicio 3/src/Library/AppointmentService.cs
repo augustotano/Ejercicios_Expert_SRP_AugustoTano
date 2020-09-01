0cddedbab966f37baaa26d58ca7d0083d5f05ba8
Augusto Taño <augusto.1812@hotmail.com>
2020-09-01 20:54:44 -0300

    Codigo horroroso, por favor corregir en base a comentarios.

--- Ejercicio 3/src/Library/AppointmentService.cs
+++ Ejercicio 3/src/Library/AppointmentService.cs
@@ -4,7 +4,11 @@ using System.Text;
 namespace Library
 {
     public class AppointmentService
-    {
+    {   
+        //Horrible, se piden muchas cosas que tendrían que estar subdivididas. Tampoco hay mucha especificación de que es cada cosa.
+        //Parametros "name", "phoneNumber" e "id" tendrían que estar en una clase que sea socio. Appointment pediría un objeto de tipo socio.
+        //Parametros "date" y "appointmentPlace" podrían conservarse dentro del metodo que lo crea.
+        //Parametros como "doctorName" serían ideal en una clase nueva con nombre doctor para cualquier futura ampliación.
         public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
         {
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
