e7378a324abfffbc08ef52e8d048182311b0e7c2
Augusto Taño <augusto.1812@hotmail.com>
2020-09-01 23:26:00 -0300

    Update Biblioteca.cs

--- Ejercicio 2/src/Library/Biblioteca.cs
+++ Ejercicio 2/src/Library/Biblioteca.cs
@@ -5,15 +5,11 @@ namespace SRP
 {
     public class Biblioteca
     {
-        //No cumple SRP. Tendrían que estar en su propia clase biblioteca.
         ArrayList ListaLibros = new ArrayList();
 
-
-        //Debería estar tambien en su propia clase llamada biblioteca.
         public void AlmacenarLibro(Libro libro, String sector, String estante)
         {
             ListaLibros.Add((libro, sector, estante));
-            Console.WriteLine(ListaLibros[0]);
         }
 
     }
