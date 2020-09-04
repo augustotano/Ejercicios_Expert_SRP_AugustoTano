0fc4401b1f79a7e49e1bd38906d13271ea48fd3e
Augusto Taño <augusto.1812@hotmail.com>
2020-09-04 19:52:43 -0300

    Update AppointmentService.cs

--- Ejercicio 3/src/Library/AppointmentService.cs
+++ Ejercicio 3/src/Library/AppointmentService.cs
@@ -9,13 +9,15 @@ namespace Library
         private Socio socio {get; set;}
         private Doctor doctor {get;set;} 
         private Clinica clinica {get;set;}
-        public string CreateAppointment(Socio socio1, Doctor doctor1, Clinica clinica1)
+        private DateTime fecha {get; set;}
+        public string CreateAppointment(Socio socio1, Doctor doctor1, Clinica clinica1, DateTime fecha1)
         {
             this.socio = socio1;
             this.doctor = doctor1;
             this.clinica = clinica1;
+            this.fecha = fecha1;
 
-            return $"Cita creada para el socio {socio1.nombre} con el doctor {doctor1.nombre} en la clinica {clinica.nombre}";
+            return $"Cita creada para el socio {socio1.nombre} con el doctor {doctor1.nombre} en la clinica {clinica.nombre} el día {this.fecha}";
             
         }
 
